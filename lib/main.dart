1e45b96b395c6603fdd6508cead0577d97b61bfb
Abdullah Al Roman <abdullahr583@gmail.com>
2019-06-29 22:15:47 +0600

    appBar dynamic Text,add More,Pupil,Message page list

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,31 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   int _selectedPage = 0;
+  String appbarTitle = "Pupil";
+  final moretabList = [
+    'Finance',
+    'Mileage',
+    'Driving Tests',
+    'Diary Setup',
+    'Message All Your Pupils',
+    'Internal Instructor Chat',
+    'Pupil Achieves',
+    'App Version 2.2',
+  ];
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test change from Jahir',
+      title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Color(hexColor('#03D1BF')),
-          title: Text("Add Pupil"),
+          title: Text(appbarTitle),
           actions: <Widget>[
             Container(
               padding: EdgeInsets.only(right: 2.0),
               child: Center(
                   child: ButtonTheme(
                 minWidth: 100.0,
-                height: 50.0,
+                height: 60.0,
                 child: RaisedButton(
                   color: Color(
                     hexColor('#03D1BF'),
@@ -48,6 +59,20 @@ class MyAppState extends State<MyApp> {
           child: IndexedStack(
             index: _selectedPage,
             children: <Widget>[
+              Container(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: ListTile(
+                        title: Text('Pipul $index'),
+                        onTap: () {},
+                      ),
+                    );
+                  },
+                  //separatorBuilder: (BuildContext context, int index) => const Divider(),
+                ),
+              ),
               Container(
                 child: Center(
                   child: Column(children: <Widget>[
@@ -225,26 +250,6 @@ class MyAppState extends State<MyApp> {
                   child: Container(
                     height: 100,
                     width: 100,
-                    color: Color(
-                      hexColor('#03D1BF'),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Add Pupil",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
                     color: Color(hexColor('#03D1BF')),
                     child: Center(
                       child: Text(
@@ -259,40 +264,38 @@ class MyAppState extends State<MyApp> {
                 ),
               ),
               Container(
-                child: Center(
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color(hexColor('#03D1BF')),
-                    child: Center(
-                      child: Text(
-                        "Message",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: ListTile(
+                        title: Text("Message $index"),
+                        onTap: () {},
                       ),
-                    ),
-                  ),
+                    );
+                  },
+                  //separatorBuilder: (BuildContext context, int index) => const Divider(),
                 ),
               ),
               Container(
-                  child: Center(
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Color(
-                    hexColor('#03D1BF'),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "More",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                  ),
+                child: ListView.builder(
+                  itemCount: moretabList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: ListTile(
+                        title: Text(moretabList[index]),
+                        onTap: () {
+                          index == 7
+                              ? dialogBox(context, 'No Update Available',
+                                  'Your version is up to date')
+                              : null;
+                        },
+                      ),
+                    );
+                  },
+                  //separatorBuilder: (BuildContext context, int index) => const Divider(),
                 ),
-              )),
+              ),
             ],
           ),
         ),
@@ -308,6 +311,7 @@ class MyAppState extends State<MyApp> {
             onTap: (int index) {
               setState(() {
                 _selectedPage = index;
+                setAppbarTitle(_selectedPage);
               });
             },
             items: [
@@ -368,167 +372,38 @@ class MyAppState extends State<MyApp> {
     );
   }
 
-  Widget pupilPage = Container(
-    child: Center(
-      child: Column(children: <Widget>[
-        //textBoxSection,
-        Container(
-          padding:
-              EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0, bottom: 10.0),
-          child: Wrap(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.only(bottom: 5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.cyan[300]),
-                              borderRadius: new BorderRadius.circular(8.0)),
-                          hintText: "First Name"),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                      padding: EdgeInsets.only(bottom: 5.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.cyan[300]),
-                                borderRadius: BorderRadius.circular(8.0)),
-                            hintText: "Last Name"),
-                      )),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.only(bottom: 5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.cyan[300]),
-                              borderRadius: BorderRadius.circular(8.0)),
-                          hintText: "Mobile No"),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.only(bottom: 5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.cyan[300]),
-                              borderRadius: BorderRadius.circular(8.0)),
-                          fillColor: Colors.greenAccent,
-                          hintText: "Email"),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-        //  smsonOfButtonSection,
-        Container(
-          padding:
-              EdgeInsets.only(top: 2.0, left: 20.0, right: 20.0, bottom: 0.0),
-          child: Row(
-            children: [
-              Expanded(
-                /*1*/
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    /*2*/
-                    Container(
-                      child: Text(
-                        'Sent Message via SMS',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              /*3*/
-              Switch(value: true, onChanged: null)
-            ],
-          ),
-        ),
-        //  emailonOfButtonSection,
-        Container(
-          padding:
-              EdgeInsets.only(bottom: 2.0, left: 20.0, right: 20.0, top: 0.0),
-          child: Row(
-            children: [
-              Expanded(
-                /*1*/
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    /*2*/
-                    Container(
-                      child: Text(
-                        'Sent Message via email',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              /*3*/
-              Switch(
-                  value: true, onChanged: null, activeColor: Colors.cyan[300])
-            ],
-          ),
-        ),
-        //  addButton,
-        Container(
-          padding: EdgeInsets.all(5.0),
-          child: Column(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  ButtonTheme(
-                    minWidth: 310.0,
-                    height: 40.0,
-                    child: RaisedButton(
-                      color: Colors.cyan[300],
-                      onPressed: () {},
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(8.0),
-                      ),
-                      child: Text(
-                        "Add",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ]),
-    ),
-  );
   hexColor(String colorhexcode) {
     String colornew = '0xff' + colorhexcode;
     colornew = colornew.replaceAll('#', '');
     int colorint = int.parse(colornew);
     return colorint;
+  }
+
+  Future<void> dialogBox(BuildContext context, String title, String message) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  setAppbarTitle(int selectedPage) {
+    selectedPage == 0 ? appbarTitle = "Pupil" : null;
+    selectedPage == 1 ? appbarTitle = "Add Pupil" : null;
+    selectedPage == 2 ? appbarTitle = "Diary" : null;
+    selectedPage == 3 ? appbarTitle = "Your Messages" : null;
+    selectedPage == 4 ? appbarTitle = "More" : null;
   }
 }
