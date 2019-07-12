8d2e9a9e539bfbf853b88a40daaa40861d4ddfa3
MD JAHIRUL ISLAM <JAHIRHSTU@GMAIL.COM>
2019-07-12 17:17:07 +0600

    Updated UI for login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,32 +9,58 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _countryCodeController = TextEditingController();
   TextEditingController _smsCodeController = TextEditingController();
   TextEditingController _phoneNumberController = TextEditingController();
   String verificationId;
 
   @override
   Widget build(BuildContext context) {
+    var screen_width = MediaQuery.of(context).size.width;
+    var screen_height = MediaQuery.of(context).size.height;
+    var one_fourth_width = screen_width / 6;
     _smsCodeController.text = '123456';
-    _phoneNumberController.text='+1 650-555-1234';
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Login Test"),
       ),
       body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new TextField(
-              controller: _phoneNumberController
-            ),
-            new TextField(
-              controller: _smsCodeController,
-            ),
-            new FlatButton(
-                onPressed: () => _signInWithPhoneNumber(_smsCodeController.text),
-                child: const Text("Sign In"))
-          ],
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: one_fourth_width, right: one_fourth_width),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new TextField(
+                controller: _countryCodeController,
+                readOnly: true,
+                enabled: false,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: "United Kingdom (+44)",
+                  hintStyle: TextStyle(color: Colors.green),
+                ),
+              ),
+              new TextField(
+                controller: _phoneNumberController,
+                keyboardType: TextInputType.number,
+                textAlignVertical: TextAlignVertical.bottom,
+                maxLength: 11,
+                decoration: InputDecoration(
+                  hintText: "Your Phone Number",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  prefixIcon: Icon(Icons.phone),
+                  alignLabelWithHint: true,
+                  labelText: "Phone Number"
+                ),
+              ),
+              new TextField(controller: _smsCodeController),
+              new FlatButton(
+                  onPressed: () =>
+                      _signInWithPhoneNumber(_smsCodeController.text),
+                  child: const Text("Sign In"))
+            ],
+          ),
         ),
       ),
       floatingActionButton: new FloatingActionButton(
@@ -63,34 +89,40 @@ class _LoginPageState extends State<LoginPage> {
 
   /// Sends the code to the specified phone number.
   Future<void> _sendCodeToPhoneNumber() async {
-    final PhoneVerificationCompleted verificationCompleted = (AuthCredential authCredential) {
+    final PhoneVerificationCompleted verificationCompleted =
+        (AuthCredential authCredential) {
       setState(() {
-        var message = 'Inside _sendCodeToPhoneNumber: signInWithPhoneNumber auto succeeded.';
+        var message =
+            'Inside _sendCodeToPhoneNumber: signInWithPhoneNumber auto succeeded.';
         print(message);
         dialogBox(context, 'Signed status', message);
       });
     };
 
-    final PhoneVerificationFailed verificationFailed = (AuthException authException) {
+    final PhoneVerificationFailed verificationFailed =
+        (AuthException authException) {
       setState(() {
-        var message = 'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}';
+        var message =
+            'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}';
         print(message);
         dialogBox(context, 'Signed status', message);
       });
     };
 
-    final PhoneCodeSent codeSent = (String verificationId, [int forceResendingToken]) async {
+    final PhoneCodeSent codeSent =
+        (String verificationId, [int forceResendingToken]) async {
       this.verificationId = verificationId;
-        var message = "code sent to " + _phoneNumberController.text;
+      var message = "code sent to " + _phoneNumberController.text;
       print(message);
-        dialogBox(context, 'Signed status', message);
+      dialogBox(context, 'Signed status', message);
     };
 
-    final PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout = (String verificationId) {
+    final PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
       this.verificationId = verificationId;
-        var message = "time out";
+      var message = "time out";
       print(message);
-        dialogBox(context, 'Signed status', message);
+      dialogBox(context, 'Signed status', message);
     };
 
     await FirebaseAuth.instance.verifyPhoneNumber(
@@ -101,7 +133,7 @@ class _LoginPageState extends State<LoginPage> {
         codeSent: codeSent,
         codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
   }
-  
+
   Future<void> dialogBox(BuildContext context, String title, String message) {
     return showDialog<void>(
       context: context,
