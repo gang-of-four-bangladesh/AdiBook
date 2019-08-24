0aa63c72f268f221dc71f3bc704490a8dd5f66be
Md Jahirul Islam <jahirhstu@gmail.com>
2019-08-24 14:11:53 +0600

    Renamed common_class file name

---
+++ lib/utils/frequent_widgets.dart
@@ -0,0 +1,53 @@
+import 'package:adibook/core/constants.dart';
+import 'package:flutter/material.dart';
+
+class FrequentWidgets {
+  getSnackbar(String message, BuildContext context) {
+    Scaffold.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        content: Text(
+          message,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: AppTheme.appThemeColor.withOpacity(0.5),
+      ),
+    );
+  }
+
+ Widget getProgressBar() {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppTheme.appThemeColor),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> dialogBox(BuildContext context, String title, String message) {
+      return showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(message),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+}
