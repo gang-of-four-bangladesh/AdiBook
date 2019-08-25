62e2125b0fc8667bb2e51a58d6c01aad805ea6b0
Abdullah Al Roman <abdullahr583@gmail.com>
2019-08-26 00:17:14 +0600

    add progress_planner_section page star inforation

--- lib/pages/instructor/pupil_activities/progress_planner_section.dart
+++ lib/pages/instructor/pupil_activities/progress_planner_section.dart
@@ -31,61 +31,136 @@ class _ProgressPlannerSectionState extends State<ProgressPlannerSection> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(
-        itemCount: _progressPlanDetails.length,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(width: 1.5, color: Colors.black12),
-              ),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: ListTile(
-                    title: Text(
-                      _progressPlanDetails[index].subjectDisplayName,
-                      style: TextStyle(
-                        fontSize: 15.0,
+        child: Column(
+      children: <Widget>[
+        Container(
+          height: 70.0,
+          color: Colors.green,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    "1. Introduced",
+                    style: TextStyle(
+                        fontSize: 15,
                         fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                        color: Colors.white),
+                  ),
+                  Text(
+                    "     2. Talk Through",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    "3. Prompted",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    "      4. Rarely Prompted",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    "5. Independent",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                   Text(
+                    "",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  )
+                ],
+              ),
+            
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _progressPlanDetails.length,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 1.5, color: Colors.black12),
                   ),
                 ),
-                SmoothStarRating(
-                  allowHalfRating: false,
-                  onRatingChanged: (v) async {
-                    setState(() {
-                      _progressPlanDetails[index].status =
-                          ProgressSubjectStatus.values[v.toInt()];
-                      print(
-                          'Rating changed for index ${_progressPlanDetails[index].subjectDisplayName} to ${_progressPlanDetails[index].status}');
-                    });
-                    var _subject = new ProgressPlanSubject(
-                      subjectName: _progressPlanDetails[index].subjectName,
-                      subjectStatus: ProgressSubjectStatus.values[v.toInt()],
-                    );
-                    await ProgressPlan(
-                      pupilId: appData.pupilId,
-                      instructorId: appData.instructorId,
-                      progressPlanSubject: _subject,
-                    ).update();
-                  },
-                  starCount: 5,
-                  rating: _progressPlanDetails[index].status.index.toDouble(),
-                  size: 35.0,
-                  color: Colors.green,
-                  borderColor: Colors.green,
-                  spacing: 0.0,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                        title: Text(
+                          _progressPlanDetails[index].subjectDisplayName,
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SmoothStarRating(
+                      allowHalfRating: false,
+                      onRatingChanged: (v) async {
+                        setState(() {
+                          _progressPlanDetails[index].status =
+                              ProgressSubjectStatus.values[v.toInt()];
+                          print(
+                              'Rating changed for index ${_progressPlanDetails[index].subjectDisplayName} to ${_progressPlanDetails[index].status}');
+                        });
+                        var _subject = new ProgressPlanSubject(
+                          subjectName: _progressPlanDetails[index].subjectName,
+                          subjectStatus:
+                              ProgressSubjectStatus.values[v.toInt()],
+                        );
+                        await ProgressPlan(
+                          pupilId: appData.pupilId,
+                          instructorId: appData.instructorId,
+                          progressPlanSubject: _subject,
+                        ).update();
+                      },
+                      starCount: 5,
+                      rating:
+                          _progressPlanDetails[index].status.index.toDouble(),
+                      size: 35.0,
+                      color: Colors.green,
+                      borderColor: Colors.green,
+                      spacing: 0.0,
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          );
-        },
-        //separatorBuilder: (BuildContext context, int index) => const Divider(),
-      ),
-    );
+              );
+            },
+            //separatorBuilder: (BuildContext context, int index) => const Divider(),
+          ),
+        )
+      ],
+    ));
   }
 }
