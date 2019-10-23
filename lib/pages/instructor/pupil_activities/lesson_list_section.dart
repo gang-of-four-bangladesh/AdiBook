4f6a269e896e33806e8d4ceb012d60f6e02ecae5
Md Jahirul Islam <jahirhstu@gmail.com>
2019-10-23 22:56:37 +0600

    lesson list updated, provided progress report for pupil

--- lib/pages/instructor/pupil_activities/lesson_list_section.dart
+++ lib/pages/instructor/pupil_activities/lesson_list_section.dart
@@ -33,7 +33,7 @@ class LessonListSectionState extends State<LessonListSection> {
   }
 
   void _loadLessonsData() async {
-    this._pupilId = appData.contextualInfo[DataSharingKeys.PupilIdKey];
+    this._pupilId = _getPupilId();
     if (!mounted) return;
     setState(() {
       _querySnapshot = PupilManager()
@@ -41,6 +41,13 @@ class LessonListSectionState extends State<LessonListSection> {
               instructorId: appData.instructor.id, pupilId: this._pupilId)
           .asStream();
     });
+  }
+
+  String _getPupilId() {
+    if (isNotNullOrEmpty(appData.pupil)) return appData.pupil.id;
+    return appData.contextualInfo ??
+        appData.contextualInfo.containsKey(DataSharingKeys.PupilIdKey) ??
+        appData.contextualInfo[DataSharingKeys.PupilIdKey];
   }
 
   @override
